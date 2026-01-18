0b4f041cefafb1e5b9b76f7bda37232fb4d3cbe4
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-01-18 14:15:55 +0530

    User signup option added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
+import User from "./models/user.js";
+
 dotenv.config();
 
 const app = express();
@@ -10,16 +12,79 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
-
-
-app.get("/" , (req ,res) => {
-    res.json({message: "Welcome to tiny tours"});
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to tiny tours" });
 });
 
-app.get("/health" , (req ,res) => {
-    res.json({status: "ok"});
+app.get("/health", (req, res) => {
+    res.json({ status: "ok" });
 });
 
-app.listen(PORT , () => {
-    console.log(`Serever is running on PORT ${PORT}`)
+app.post("/signup", async (req, res) => {
+    const { name, email, mobile, city, country, password } = req.body;
+
+    if (!name) {
+        return res.json({
+            success: false,
+            message: "Name is required",
+            data: null,
+        });
+    }
+
+    if (!email) {
+        return res.json({
+            success: false,
+            message: "Email is required",
+            data: null,
+        });
+    }
+
+    if (!password) {
+        return res.json({
+            success: false,
+            message: "Password is required",
+            data: null,
+        });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        return res.json({
+            success: false,
+            message: "User with this email already exists",
+            data: null,
+        });
+    }
+
+    const newUser = new User({
+        name,
+        email,
+        mobile,
+        city,
+        country,
+        password,
+    });
+
+    try {
+        const savedUser = await newUser.save();
+
+        return res.json({
+            success: true,
+            message: "User registered successfully",
+            data: savedUser,
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: `User registration failed: ${error.message}`,
+            data: null,
+        });
+    }
+});
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Serever is running on PORT ${PORT}`);
 });
