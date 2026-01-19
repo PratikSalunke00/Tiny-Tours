f566ffbcb5af304372127e8460da620dc9e14702
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-01-19 14:55:15 +0530

    referencing in mongoDB

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import connectDB from "./db.js";
 import User from "./models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import Tour from "./models/Tour.js";
 
 dotenv.config();
 
@@ -13,6 +15,24 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
+const checkJWT = (req, res, next) => {
+    const { authorization } = req.headers;
+    const token = authorization && authorization.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        console.log("Decoded JWT :", decoded);
+        next();
+    } catch (err) {
+        return res.json({
+            success: false,
+            message: "Invalid or missing token",
+            data: null,
+        });
+    }
+};
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to tiny tours" });
 });
@@ -20,6 +40,7 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
     res.json({ status: "ok" });
 });
+
 
 app.post("/signup", async (req, res) => {
     const { name, email, mobile, city, country, password } = req.body;
@@ -87,7 +108,6 @@ app.post("/signup", async (req, res) => {
     }
 });
 
-
 app.post("/login", async (req, res) => {
     const { email, password } = req.body;
     if (!email) {
@@ -118,12 +138,25 @@ app.post("/login", async (req, res) => {
 
     const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
     existingUser.password = undefined;
-    
+
     if (isPasswordCorrect) {
+        const jwtToken = jwt.sign(
+            {
+                id: existingUser.id,
+                email: existingUser.email,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h",
+            }
+        );
+
+
         return res.json({
             success: true,
             message: "Login successful",
             data: existingUser,
+            jwtToken: jwtToken,
         });
     } else {
         return res.json({
@@ -132,6 +165,50 @@ app.post("/login", async (req, res) => {
             data: null,
         });
     }
+});
+
+app.post("/tours", checkJWT, async (req, res) => {
+    const { title, description, cities, startDate, endDate, photos } =
+        req.body;
+
+    const newTour = new Tour({
+        title,
+        description,
+        cities,
+        startDate,
+        endDate,
+        photos,
+        user: req.user.id,
+    });
+
+    try {
+        const savedTour = await newTour.save();
+
+        return res.json({
+            success: true,
+            message: "Tour created successfully",
+            data: savedTour,
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: `Tour creation failed: ${error.message}`,
+            data: null,
+        });
+    }
+});
+
+app.get("/tours", checkJWT, async (req, res) => {
+    const tours = await Tour.find({ user: req.user.id }).populate(
+        "user",
+        "-password"
+    );
+
+    return res.json({
+        success: true,
+        message: "Fetched tours successfully",
+        data: tours,
+    });
 });
 
 connectDB();
